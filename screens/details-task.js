257c0846d1886ff7fb83c79f1561b2ed15f67262
Mauricio Yepez <mrodriguez072@accitesz.com>
2020-03-27 12:02:36 -0600

    CheckBox succesfull
    Ya funcionan los checkbox

--- screens/details-task.js
+++ screens/details-task.js
@@ -1,21 +1,61 @@
 import React, { Component } from 'react'
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, CheckBox } from 'react-native';
 import tasks from '../samples/taskexamples.json';
 import Card from '../tickets/Card'
 
+//Variable temporal para mi checkbox de su valor True o False 
+var tempCheckValues = [];
+
 class Details extends Component {
+
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      checkBoxChecked: []
+    }
+    this.valuesCheckBox();
+  }
   
   // ** u can write code js here **
   example = { 
     task: tasks 
   }
 
+  valuesCheckBox()
+  {
+    this.example.task.map((val) => {
+      tempCheckValues[val.id] = false
+    })
+  }
+
+  mChangeValueCheckBox(id,value)
+  {
+    this.setState({
+      checkBoxChecked: tempCheckValues
+    })
+
+    var tempCheckBoxChecked = this.state.checkBoxChecked;
+    tempCheckBoxChecked[id] = !value;
+
+    this.setState({
+      checkBoxChecked: tempCheckBoxChecked
+    })
+
+  }
+
+
   render(){
     return (
-      <View>
+      <View >
         { this.example.task.map(e => 
-          <View> 
+        
+          <View key={e.id}> 
             <Card>
+              <CheckBox
+                value = { this.state.checkBoxChecked[e.id]}
+                onValueChange = { () => this.mChangeValueCheckBox(e.id, this.state.checkBoxChecked[e.id])}
+              />
               <Text>  { e.task } </Text> 
               <Text> { e.description }</Text>
             </Card>
--- tickets/Card.js
+++ tickets/Card.js
@@ -3,35 +3,34 @@ import { StyleSheet, View } from 'react-native';
 
 
 export default function Card(props) {
-
+    
+ 
     return(
-
-        <View>
-            <View>
+        <View style={ styles.card }>
+            <View style={ styles.cardContent }>
                 { props.children }
             </View>
         </View>
     )
 
 }
-
 const styles = StyleSheet.create({
     card: {
-        borderRadius: 6,
-        elevation: 3,
+        borderRadius: 30,
+        elevation: 5,
         backgroundColor: '#FFF',
         shadowOffset: { width: 1, height: 1 },
         shadowColor: '#333',
         shadowOpacity: 0.3,
         shadowRadius: 2,
         marginHorizontal: 4,
-        marginVertical: 6
+        marginVertical: 6,
+        
     },
 
-    cardConten: {
-        marginHorizontal: 18,
+    cardContent: {
+        flexDirection: "row",
+        marginHorizontal: 40,
         marginVertical: 10
     }
 })
-
-<
